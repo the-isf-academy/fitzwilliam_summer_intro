ab10ceba4411b9dd8b305aa632b67b64ef9f99dd
ms.brown <eqbrown@isf.edu.hk>
2026-05-04 14:37:08 +0800

    clarify exponents

--- file2_for_loops.py
+++ file2_for_loops.py
@@ -6,7 +6,8 @@
 #       Maybe 5 or 14. Can you figure out how to do it?
 
 # 💻 Edit the loop again to print the square of numbers 0-12. 
-#   TO square a number you can use **X
+#   For exponents you can use use: X**Y
+#         x= base, y=power
 #
 # Go to file3.py when you are finished!
 # ////////////////////////////////////////////
